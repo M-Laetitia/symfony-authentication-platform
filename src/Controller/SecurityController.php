f96aaca3296062a8aa031c04adda0e005d78e2d5
M-Laetitia <molin.laetitia@gmail.com>
2025-07-04 14:59:02 +0200

    Refactor: add use in SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Security\OAuthFormHandler;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use App\Security\AppAuthenticator;
+
 
 class SecurityController extends AbstractController
 {
@@ -21,6 +26,39 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
+        ]);
+
+
+    }
+
+    #[Route(path: '/connect/registration/', name: 'hwi_oauth_connect_registration')]
+    public function registerWithGoogle(
+        Request $request,
+        OAuthFormHandler $formHandler,
+        UserAuthenticatorInterface $authenticator,
+        AppAuthenticator $appAuthenticator
+    ): Response {
+        $form = $this->createForm(RegistrationFormType::class);
+        $form->handleRequest($request);
+
+        /** @var UserResponseInterface $userInformation */
+        $userInformation = $request->attributes->get('hwi_oauth.user_information');
+
+        if ($formHandler->process($request, $form, $userInformation)) {
+            $user = $form->getData();
+
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $authenticator->authenticateUser(
+                $user,
+                $appAuthenticator,
+                $request
+            );
+        }
+
+        return $this->render('registration/register_with_google.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
