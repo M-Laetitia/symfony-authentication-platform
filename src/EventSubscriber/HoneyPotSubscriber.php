4d0a65b5ff5b11f54d4b533ce8dc6ae59b4eda70
M-Laetitia <molin.laetitia@gmail.com>
2025-07-11 13:34:43 +0200

    Feat: implement HoneyPotSubscriber for bot detection in form submissions

---
+++ src/EventSubscriber/HoneyPotSubscriber.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class HoneyPotSubscriber implements EventSubscriberInterface
+{
+    private LoggerInterface $logger;
+    
+    private RequestStack $requestStack;
+
+    public function __construct(LoggerInterface $logger, RequestStack $requestStack)
+    {
+        $this->logger = $logger;
+        $this->requestStack = $requestStack;
+    }
+
+    public static function getSubscribedEvents(): array {
+
+        // lors du subscribe - donc l'event pre_submit est fired > on appelle en callback checkHoneyJar > checkHoneyJar prend un formEvents > dans cet form event on récupère les datas qui ont été saisies
+        return [
+            FormEvents::PRE_SUBMIT => 'checkHoneyJar'
+        ];
+    }
+
+    public function checkHoneyJar(FormEvent $event): void {
+
+        // on récupère la request courrante (pour l'adresse ip)
+        $request = $this->requestStack->getCurrentRequest();
+
+        if(!$request) {
+            return;
+        }
+        // dd($event);
+
+        // $form = $event->getForm();
+        // on récupère les datas
+        $data = $event->getData();
+
+        if(!array_key_exists('phone', $data) || !array_key_exists('faxNumber', $data)) {
+            throw new HttpException(400, 'Go away to my form');
+        }
+
+        // destructuring
+        [
+            'phone' => $phone,
+            'faxNumber' => $faxNumber
+        ] = $data;
+        
+        // if($date['phone'])
+        if($phone !== '' || $faxNumber !== '') {
+            $this->logger->error("Potential spam attempt from a robot at the following ip address: '{$request->getClientIp()}'");
+            $this->logger->info("The data in the phone field contained '{$phone}', and the fax field contained '{$faxNumber}'");
+            
+            throw new HttpException(403, 'Go away to my form, bot !');
+        }
+    }
+}
