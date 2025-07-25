1018c356a045f29c46191d9767d7f47dbb24ae5e
M-Laetitia <molin.laetitia@gmail.com>
2025-07-25 16:16:59 +0200

    Clean:  remove commented code

--- tests/Service/OAuthConnectorTest.php
+++ tests/Service/OAuthConnectorTest.php
@@ -42,22 +42,5 @@ class OAuthConnectorTest extends TestCase
 
     // A FAIRE : le cas où le provider (google) n'est pas dans le tableau de $properties cela doit faire un return sans rien faire , test à faire : on vérifie que persist() et flush() ne sont pas appelés
 
-    
 
-    // public function testConnectDoesNothingIfNoMapping(): void
-    // {
-    //     $user = $this->createMock(UserInterface::class);
-    //     $response = $this->createMock(UserResponseInterface::class);
-    //     $resourceOwner = $this->createMock(\HWI\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface::class);
-
-    //     $response->method('getResourceOwner')->willReturn($resourceOwner);
-    //     $resourceOwner->method('getName')->willReturn('github'); // Pas dans le mapping
-
-    //     $entityManager = $this->createMock(EntityManagerInterface::class);
-    //     $entityManager->expects($this->never())->method('persist');
-    //     $entityManager->expects($this->never())->method('flush');
-
-    //     $connector = new OAuthConnector($entityManager, ['google' => 'googleId']);
-    //     $connector->connect($user, $response);
-    // }
 }
