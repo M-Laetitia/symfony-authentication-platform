a3b173e8d7ef2008d095c5b211e2ed337d30593b
M-Laetitia <molin.laetitia@gmail.com>
2025-09-26 12:08:58 +0200

    Feat: add editor.js

---
+++ assets/js/editor.js
@@ -0,0 +1,27 @@
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import Quote from '@editorjs/quote';
+import SimpleImage from '@editorjs/simple-image';
+
+const editor = new EditorJS({
+    holder: 'editorjs',
+    tools: {
+        header: Header,
+        list: List,
+        quote: Quote,
+        image: SimpleImage,
+    },
+    data: window.articleContent || {}, // charge le contenu existant depuis Twig
+});
+
+// gérer le submit du formulaire
+const form = document.getElementById('articleForm');
+if (form) {
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const outputData = await editor.save();
+        document.getElementById('article_content').value = JSON.stringify(outputData);
+        form.submit();
+    });
+}
