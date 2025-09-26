cd8a7fee1b9524884dfc216d28de76307806f411
M-Laetitia <molin.laetitia@gmail.com>
2025-09-26 12:06:53 +0200

    feat: create ArticleFormType for article management

---
+++ src/Form/ArticleFormType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Tag;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('excerpt')
+            ->add('metaTitle')
+            ->add('metaDescription')
+
+            // ->add('category', EntityType::class, [
+            //     'class' => Category::class,
+            //     'choice_label' => 'id',
+            // ])
+            // ->add('tags', EntityType::class, [
+            //     'class' => Tag::class,
+            //     'choice_label' => 'id',
+            //     'multiple' => true,
+            // ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
