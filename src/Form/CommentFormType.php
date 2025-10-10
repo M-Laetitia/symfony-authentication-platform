4e03c0773078f1c14dc2254c3240f26fbf0b2efc
M-Laetitia <molin.laetitia@gmail.com>
2025-10-10 09:38:37 +0200

    feat: conditionally add author name field in comment form based on user authentication

--- src/Form/CommentFormType.php
+++ src/Form/CommentFormType.php
@@ -8,18 +8,28 @@ use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bundle\SecurityBundle\Security;
 
 
 class CommentFormType extends AbstractType
 {
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+    
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content', TextareaType::class)
-            ->add('authorName', TextareaType::class);
+            ->add('content', TextareaType::class);
+            // ->add('authorName', TextareaType::class);
             // ->add('author', EntityType::class, [
             //     'class' => User::class,
             //     'choice_label' => 'id',
@@ -30,7 +40,18 @@ class CommentFormType extends AbstractType
             //         'label' => 'Votre nom',
             //     ]);
             // }
-        
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+            
+            // Si l'utilisateur n'est pas connecté, on ajoute le champ pseudo
+            if (!$this->security->getUser()) {
+                $form->add('authorName', TextareaType::class, [
+                    'label' => 'Votre pseudo',
+                    'mapped' => false, 
+                    'required' => true
+                ]);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
