a6ff0b47427291e1fa94ef143eb477342f736170
M-Laetitia <molin.laetitia@gmail.com>
2025-10-17 13:27:32 +0200

    fix: correct path for editor entry in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('editor', './assets/js/editor.js')
+    .addEntry('editor', './assets/js/editorjs/editor.js')
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
