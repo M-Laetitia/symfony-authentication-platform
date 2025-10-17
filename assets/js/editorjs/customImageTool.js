b947b12059237bf1ea9a2acc0efebb04ebcaeb8c
M-Laetitia <molin.laetitia@gmail.com>
2025-10-17 17:11:01 +0200

    Feat: add an inout to write a texte for the altText for images

--- assets/js/editorjs/customImageTool.js
+++ assets/js/editorjs/customImageTool.js
@@ -1,6 +1,30 @@
 import ImageTool from '@editorjs/image';
 
 export default class CustomImageTool extends ImageTool {
+
+    render() {
+        const wrapper = super.render();
+        
+        // Créer le champ pour l'alt text
+        const altTextWrapper = document.createElement('div');
+        altTextWrapper.classList.add('cdx-input', 'image-tool__alt-text');
+        
+        const altTextInput = document.createElement('input');
+        altTextInput.placeholder = 'Texte descriptif de l\'image (alt text)';
+        altTextInput.value = this.data.alt || '';
+        altTextInput.classList.add('cdx-input');
+        altTextInput.setAttribute('name', 'alt');
+        
+        // Écouter les changements
+        altTextInput.addEventListener('input', (e) => {
+            this.data.alt = e.target.value;
+        });
+        
+        altTextWrapper.appendChild(altTextInput);
+        wrapper.appendChild(altTextWrapper);
+        
+        return wrapper;
+    }
     save(blockContent) {
         // console.log('Avant modification:', blockContent); 
         debugger; 
