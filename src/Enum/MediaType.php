cd13159c4146b641ff88445299ac2305d251ae4e
M-Laetitia <molin.laetitia@gmail.com>
2025-10-24 17:33:38 +0200

    Fix: correct case name for article cover in MediaType enum

--- src/Enum/MediaType.php
+++ src/Enum/MediaType.php
@@ -4,7 +4,8 @@ namespace App\Enum;
 
 enum MediaType: string
 {
-    case COVER = 'cover';
+    case ARTICLE_COVER = 'aticle_cover';
     case GALLERY_IMAGE = 'gallery_image';
     case ARTICLE_IMAGE = 'article_image';
+    case DEFAULT = 'default'; 
 }
