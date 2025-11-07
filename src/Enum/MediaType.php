6c8741118361965a1fdff8827a85b001a3f8fb68
M-Laetitia <molin.laetitia@gmail.com>
2025-11-07 09:11:02 +0100

    Fix: correct typo in MediaType enum for ARTICLE_COVER case

--- src/Enum/MediaType.php
+++ src/Enum/MediaType.php
@@ -4,7 +4,7 @@ namespace App\Enum;
 
 enum MediaType: string
 {
-    case ARTICLE_COVER = 'aticle_cover';
+    case ARTICLE_COVER = 'article_cover';
     case GALLERY_IMAGE = 'gallery_image';
     case ARTICLE_IMAGE = 'article_image';
     case DEFAULT = 'default'; 
