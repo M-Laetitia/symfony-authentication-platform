8e20f23c3d0b4a8fda6e1662707b2fb42cece80a
M-Laetitia <molin.laetitia@gmail.com>
2025-11-07 13:37:38 +0100

    Feat: add automatic flash message handling on page load

---
+++ assets/js/global.js
@@ -0,0 +1,14 @@
+// Gestion automatique des messages flash
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("hello")
+    const flashes = document.querySelectorAll('.flash-message');
+    if (!flashes.length) return;
+
+    setTimeout(() => {
+        flashes.forEach(element => {
+            element.style.transition = 'opacity 0.5s';
+            element.style.opacity = '0';
+            setTimeout(() => element.remove(), 500);
+        });
+    }, 3000);
+});
