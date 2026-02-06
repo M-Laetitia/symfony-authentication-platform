6af6c2191a013d28b68c0950cb94a84c861a5139
M-Laetitia <molin.laetitia@gmail.com>
2026-02-06 18:05:00 +0100

    Add SectionIntro component with template and styling

---
+++ src/Twig/Components/SectionIntro.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class SectionIntro
+{
+    public string $title = 'Title';
+    public string $subtile = 'Subtile';
+    public string $buttonText = 'See more';
+    public ?string $variant = null;
+}
