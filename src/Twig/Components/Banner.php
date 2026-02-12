77cc6cb4f5ce6e05d389f1267db3e979051fb71e
M-Laetitia <molin.laetitia@gmail.com>
2026-02-12 17:01:22 +0100

    Add Banner component: implement PHP class and corresponding Twig template for page hero

---
+++ src/Twig/Components/Banner.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class Banner
+{
+    public string $title = 'Title';
+    public string $subtile = 'Subtile';
+    public ?string $imageSrc = '';
+    public ?string $variant ='';
+}
