776723fa9e515a5880abdf8d1076e6dc4919c8f4
M-Laetitia <molin.laetitia@gmail.com>
2026-02-18 21:10:45 +0100

    Feat: allow user to change their password on their profile page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,14 +8,21 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Form\ProfileFormType;
+use App\Form\ProfileChangePasswordFormType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Form\FormError;
+
+
+
 
 class UserController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
     #[IsGranted('ROLE_USER')]
-    public function profile(Request $request,  EntityManagerInterface $entityManager): Response
+    public function profile(Request $request,  EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         // Récupère l'utilisateur connecté via AbstractController
+        /** @var \App\Entity\User $user */
         $user = $this->getUser();
 
         // Si aucun utilisateur => redirection vers login
@@ -27,7 +34,7 @@ class UserController extends AbstractController
         //     throw $this->createAccessDeniedException('Accès refusé : rôle insuffisant.');
         // }
 
-
+        //^ FORM INFO
         $profilInfoForm = $this->createForm(ProfileFormType::class, $user);
         $profilInfoForm->handleRequest($request);
         if ($profilInfoForm->isSubmitted() && $profilInfoForm->isValid()) {
@@ -39,10 +46,31 @@ class UserController extends AbstractController
             return $this->redirectToRoute('app_profile');
         }
         
-        // Rend la vue avec l'utilisateur
+        //^ Form password
+        $formPassword = $this->createForm(ProfileChangePasswordFormType::class);
+        $formPassword->handleRequest($request);
+
+        if ($formPassword->isSubmitted() && $formPassword->isValid()) {
+
+            $currentPassword = $formPassword->get('currentPassword')->getData();
+            $newPassword = $formPassword->get('newPassword')->getData();
+
+            if (!$passwordHasher->isPasswordValid($user, $currentPassword)) {
+                $formPassword->get('currentPassword')->addError(new FormError('Current password is incorrect.'));
+            } else {
+                $user->setPassword($passwordHasher->hashPassword($user, $newPassword));
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Password changed successfully.');
+                return $this->redirectToRoute('app_profile');
+            }
+        }
+
+
         return $this->render('user/profile.html.twig', [
             'user' => $user,
             'profilInfoForm' => $profilInfoForm->createView(),
+            'formPassword' => $formPassword->createView(),
         ]);
     }
 }
