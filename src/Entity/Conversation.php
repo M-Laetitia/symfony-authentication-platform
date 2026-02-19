d2726cec833de2a28af13464fa4351d7f1e2987d
M-Laetitia <molin.laetitia@gmail.com>
2026-02-19 09:37:38 +0100

    Feat: creation of the Conversation Entity

---
+++ src/Entity/Conversation.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\ConversationType;
+use App\Repository\ConversationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConversationRepository::class)]
+class Conversation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $creation_date = null;
+
+    #[ORM\Column(type: Types::SIMPLE_ARRAY, enumType: ConversationType::class)]
+    private array $status = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreationDate(): ?\DateTimeImmutable
+    {
+        return $this->creation_date;
+    }
+
+    public function setCreationDate(\DateTimeImmutable $creation_date): static
+    {
+        $this->creation_date = $creation_date;
+
+        return $this;
+    }
+
+    /**
+     * @return ConversationType[]
+     */
+    public function getStatus(): array
+    {
+        return $this->status;
+    }
+
+    public function setStatus(array $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
