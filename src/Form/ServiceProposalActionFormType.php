f2cb273852bad82ba1d1c0bfc89ca99cb244e8bb
M-Laetitia <molin.laetitia@gmail.com>
2026-02-22 18:45:22 +0100

    Feat: create the service proposal action form

---
+++ src/Form/ServiceProposalActionFormType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ServiceProposalActionFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        // Boutons pour accepter ou refuser
+        $builder
+            ->add('accept', SubmitType::class, [
+                'label' => 'Accept',
+                'attr' => ['class' => 'btn btn-success'],
+            ])
+            ->add('refuse', SubmitType::class, [
+                'label' => 'Refuse',
+                'attr' => ['class' => 'btn btn-danger'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
+
+// class ProposalActionType extends AbstractType
+// {
+//     public function buildForm(FormBuilderInterface $builder, array $options): void
+//     {
+//     }
+// }
