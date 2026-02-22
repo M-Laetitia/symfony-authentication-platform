fc60a9acd683df2109f422d776160402a02b7851
M-Laetitia <molin.laetitia@gmail.com>
2026-02-22 18:44:28 +0100

    Feat: implement proposal creation and action handling in ConversationController

--- src/Controller/ConversationController.php
+++ src/Controller/ConversationController.php
@@ -2,21 +2,34 @@
 
 namespace App\Controller;
 
-use App\Entity\Message;
-use App\Enum\MessageType;
-use App\Entity\Conversation;
-use App\Form\MessageFormType;
-use App\Form\ReportMessageFormType;
-use App\Repository\MessageRepository;
-use Symfony\Component\Mercure\Update;
+// Vendors
 use Doctrine\ORM\EntityManagerInterface;
-use App\Repository\ConversationRepository;
-use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+// App
+use App\Entity\Conversation;
+use App\Entity\Message;
+use App\Entity\ServiceProposal;
+
+use App\Enum\MessageType;
+use App\Enum\ServiceProposalType;
+
+use App\Form\MessageFormType;
+use App\Form\ReportMessageFormType;
+use App\Form\ServiceProposalActionFormType;
+use App\Form\ServiceProposalFormType;
+
+use App\Repository\ConversationRepository;
+use App\Repository\MessageRepository;
+use App\Repository\PhotographRepository;
+use App\Repository\TaxRepository;
+
 use App\Service\MailerService;
 
 class ConversationController extends AbstractController
@@ -62,8 +75,7 @@ class ConversationController extends AbstractController
         EntityManagerInterface $em,
         HubInterface $hub, 
         Request $request, 
-    ): Response
-    {
+    ): Response {
         $user = $this->getUser();
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
@@ -71,8 +83,27 @@ class ConversationController extends AbstractController
             throw $this->createAccessDeniedException();
         }
 
-        $messages = $messageRepo->findByConversation($conversation);
+        // $messages = $messageRepo->findByConversation($conversation);
+        $messages = $messageRepo->findByConversationWithProposals($conversation);
         $otherParticipant = $conversationRepo->findOtherParticipant($conversation, $user);
+        
+        // display accept/refuse form
+        $proposalActionForms = [];
+        foreach ($messages as $msg) {
+            if ($msg->getServiceProposal()) {
+                $proposal = $msg->getServiceProposal();
+                $proposalActionForms[$proposal->getId()] = $this->createForm(
+                    ServiceProposalActionFormType::class,
+                    null,
+                    [
+                        'action' => $this->generateUrl('proposal_action', [
+                            'id' => $proposal->getId()
+                        ]),
+                        'method' => 'POST'
+                    ]
+                )->createView();
+            }
+        }
 
         // report message 
         $reportForm = $this->createForm(ReportMessageFormType::class);
@@ -120,6 +151,36 @@ class ConversationController extends AbstractController
             'otherParticipant' => $otherParticipant,
             'form'=> $form->createView(),
             'reportForm' => $reportForm->createView(),
+            'proposalActionForms' => $proposalActionForms,
+        ]);
+    }
+
+    #[Route('/proposal/{id}/action', name: 'proposal_action', methods: ['POST'])]
+    public function proposalAction(
+        ServiceProposal $proposal,
+        Request $request,
+        EntityManagerInterface $em
+    ): Response
+    {
+        $form = $this->createForm(ServiceProposalActionFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var ClickableInterface $acceptButton */
+            $acceptButton = $form->get('accept');
+            /** @var ClickableInterface $refuseButton */
+            $refuseButton = $form->get('refuse');
+        
+            if ($acceptButton->isClicked()) {
+                $proposal->setStatus(ServiceProposalType::ACCEPTED);
+            } elseif ($refuseButton->isClicked()) {
+                $proposal->setStatus(ServiceProposalType::REJECTED);
+            }
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('chat_conversation_show', [
+            'id' => $proposal->getConversation()->getId(),
         ]);
     }
 
@@ -170,4 +231,96 @@ class ConversationController extends AbstractController
         // Si le formulaire pas valide, retourne une erreur
         return new Response('Formulaire invalide', 400);
     }
+
+
+    #[Route('/conversation/{id}/proposal/new', name: 'proposal_new')]
+    #[IsGranted('ROLE_PHOTOGRAPH')]
+    public function createProposal(
+        Conversation $conversation,
+        ConversationRepository $conversationRepo,
+        PhotographRepository $photographRepo ,
+        Request $request, 
+        EntityManagerInterface $em,
+        TaxRepository $taxRepository,
+    ): Response {
+
+        $this->denyAccessUnlessGranted('ROLE_PHOTOGRAPH');
+    
+        $proposal = new ServiceProposal();
+        $proposal->setConversation($conversation);
+
+        $activeTaxes = $taxRepository->findBy(['active' => 1]);
+
+        $form = $this->createForm(ServiceProposalFormType::class, $proposal, [
+            'active_taxes' => $activeTaxes, 
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $photographUser = $this->getUser();
+            $photograph = $photographRepo->findOneBy(['user' => $photographUser]);
+            $client = $conversationRepo->findOtherParticipant($conversation, $photographUser);
+
+            $proposal->setPhotograph($photograph);
+            $proposal->setClient($client);
+            $proposal->setCreatedAt(new \DateTimeImmutable());
+            $proposal->setStatus(ServiceProposalType::PENDING);
+
+            // Création du message lié
+            $message = new Message();
+            $message->setConversation($conversation);
+            $message->setSender($photographUser);
+            $message->setServiceProposal($proposal);
+            $message->setStatus(MessageType::UNREAD);
+            $message->setCreationDate(new \DateTimeImmutable());
+            $em->persist($proposal);
+            $em->persist($message);
+            $em->flush();
+
+            $this->addFlash('success', 'Proposal sent successfully.');
+
+            return $this->redirectToRoute('chat_conversation_show', [
+                'id' => $conversation->getId(),
+            ]);
+        }
+
+        return $this->render('proposal/new.html.twig', [
+            'form' => $form->createView(),
+            'conversation' => $conversation,
+        ]);
+    }
+
+
+    #[Route('/proposal/{id}/accept', name: 'proposal_accept', methods: ['POST'])]
+    public function accept(
+        ServiceProposal $proposal,
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+
+        $form = $this->createForm(ServiceProposalActionFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $proposal->setStatus(ServiceProposalType::ACCEPTED);
+            $em->flush();
+
+            $this->addFlash('success', 'Proposal accepted successfully.');
+
+            return $this->redirectToRoute('chat_conversation_show', [
+                'id' => $proposal->getConversation()->getId(),
+            ]);
+        }
+
+        return $this->redirectToRoute('chat_conversation_show', [
+            'id' => $proposal->getConversation()->getId(),
+        ]);
+
+        // redirection vers la page de paiement
+        // return $this->redirectToRoute('proposal_payment', ['id' => $proposal->getId()]);
+    }
 } 
+
