db360b7b52390a217009772fa438f36babe3a835
M-Laetitia <molin.laetitia@gmail.com>
2026-02-23 20:41:45 +0100

    Feat: add checkSubmissionTime for the contact form to block bot

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,18 +8,32 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\CommentSecurityService;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function contact(Request $request, MailerService $mailerService): Response
+    public function contact(Request $request, MailerService $mailerService, CommentSecurityService $CommentSecurityService): Response
     {
         $form = $this->createForm(ContactFormType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
+
+            $submittedAt = (int)$form->get('submittedAt')->getData();
+            $timeCheck = $CommentSecurityService->checkSubmissionTime($submittedAt, $request);
+        
+            if (!$timeCheck['valid']) {
+
+                if ($timeCheck['status'] === 'too_fast') {
+                    $this->addFlash('warning', 'Please wait a few seconds before submitting.');
+                    return $this->redirectToRoute('contact');
+                }
             
+                return $this->redirectToRoute('contact');
+            }
+        
             $mailerService->sendContactEmail(
                 $data['name'], 
                 $data['emailFrom'],    
