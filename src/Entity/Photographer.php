cb088545a0135fb4a8367f17ac6ecfe09a75abe4
M-Laetitia <molin.laetitia@gmail.com>
2026-02-24 20:59:56 +0100

    Feat(Photographer): add conversations relationship and methods for managing conversations

--- src/Entity/Photographer.php
+++ src/Entity/Photographer.php
@@ -31,9 +31,16 @@ class Photographer
     #[ORM\OneToMany(targetEntity: ServiceProposal::class, mappedBy: 'photographer')]
     private Collection $serviceProposals;
 
+    /**
+     * @var Collection<int, Conversation>
+     */
+    #[ORM\OneToMany(targetEntity: Conversation::class, mappedBy: 'photographer')]
+    private Collection $conversations;
+
     public function __construct()
     {
         $this->serviceProposals = new ArrayCollection();
+        $this->conversations = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -101,6 +108,36 @@ class Photographer
             // set the owning side to null (unless already changed)
             if ($serviceProposal->getPhotographer() === $this) {
                 $serviceProposal->setPhotographer(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Conversation>
+     */
+    public function getConversations(): Collection
+    {
+        return $this->conversations;
+    }
+
+    public function addConversation(Conversation $conversation): static
+    {
+        if (!$this->conversations->contains($conversation)) {
+            $this->conversations->add($conversation);
+            $conversation->setPhotographer($this);
+        }
+
+        return $this;
+    }
+
+    public function removeConversation(Conversation $conversation): static
+    {
+        if ($this->conversations->removeElement($conversation)) {
+            // set the owning side to null (unless already changed)
+            if ($conversation->getPhotographer() === $this) {
+                $conversation->setPhotographer(null);
             }
         }
 
