dd15032fef86c529f6440d9427fd1c39f4e69121
M-Laetitia <molin.laetitia@gmail.com>
2026-02-26 16:36:56 +0100

    Feat: add a new enum for the serviceProposalType (awaiting_payment)

--- src/Enum/ServiceProposalType.php
+++ src/Enum/ServiceProposalType.php
@@ -5,6 +5,7 @@ namespace App\Enum;
 enum ServiceProposalType: string
 {
     case PENDING = 'pending';
+    case AWAITING_PAYMENT = 'awaiting_payment';
     case ACCEPTED = 'accepted';
     case REJECTED = 'rejected';
 }
