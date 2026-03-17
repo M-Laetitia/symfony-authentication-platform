0658946a360645d1bcdf81d6f7d22ce6e2e2c627
M-Laetitia <molin.laetitia@gmail.com>
2026-03-17 21:29:54 +0100

    fix: update profile page header text to correct capitalization

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -74,7 +74,7 @@ class UserControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
 
         // vérifie qu'un élément spécifique est présent
-        $this->assertSelectorTextContains('h1', 'My profile');
+        $this->assertSelectorTextContains('h1', 'My Profile');
 
         $this->entityManager->remove($user);
         $this->entityManager->flush();
