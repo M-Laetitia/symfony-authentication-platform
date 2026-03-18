db9e2e3105640cd2f6bc8a083336aad1abdcb47d
M-Laetitia <molin.laetitia@gmail.com>
2026-03-18 19:55:38 +0100

    feat: implement GLightbox and Isotope functionality in separate module

--- assets/app.js
+++ assets/app.js
@@ -8,28 +8,9 @@ import './bootstrap.js';
 import './styles/app.css';
 import './js/global.js';
 
-import GLightbox from 'glightbox';
-import Isotope from 'isotope-layout';
-
-import "glightbox/dist/css/glightbox.css";
-// GLightbox init
-const lightbox = GLightbox({
-    selector: '.js-gallery-lightbox'
-});
-// Isotope init
-const iso = new Isotope('.js-gallery-grid', {
-    itemSelector: '.js-gallery-item',
-    layoutMode: 'fitRows'
-});
-// Filter buttons
-document.querySelectorAll('.js-filter-button').forEach(button => {
-    button.addEventListener('click', () => {
-        const filter = button.getAttribute('data-filter');
-        iso.arrange({ filter: filter });
-    });
-});
-
-
+if (document.querySelector('.gallery-glightbox-isotope')) {
+    import('./js/glightbox-isotope.js');
+}
 
 if (document.querySelector('#conversation-page')) {
     import('./js/chat-mercure.js');
