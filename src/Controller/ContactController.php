62a4dfe81374e4b443cf8b020e7966d5e64b643b
M-Laetitia <molin.laetitia@gmail.com>
2026-04-10 18:31:52 +0200

    feat: refactor contact form handling to use ContactFormHandler service

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,47 +3,30 @@
 namespace App\Controller;
 
 use App\Form\ContactFormType;
-use App\Service\MailerService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\CommentSecurityService;
 use App\Service\SeoService;
+use App\Service\ContactFormHandler;
+
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function contact(Request $request, MailerService $mailerService, CommentSecurityService $CommentSecurityService, SeoService $seoService): Response
+    public function contact(Request $request, SeoService $seoService, ContactFormHandler $handler ): Response
     {
         $form = $this->createForm(ContactFormType::class);
-        $form->handleRequest($request);
+        $result = $handler->handle($form, $request);
+   
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-
-            $submittedAt = (int)$form->get('submittedAt')->getData();
-            $timeCheck = $CommentSecurityService->checkSubmissionTime($submittedAt, $request);
-        
-            if (!$timeCheck['valid']) {
-
-                if ($timeCheck['status'] === 'too_fast') {
-                    $this->addFlash('warning', 'Please wait a few seconds before submitting.');
-                    return $this->redirectToRoute('contact');
-                }
-            
-                return $this->redirectToRoute('contact');
-            }
-        
-            $mailerService->sendContactEmail(
-                $data['name'], 
-                $data['emailFrom'],    
-                $data['message'],
-                $data['subject']
-            );
-
+        if ($result['success']) {
             $this->addFlash('success', 'Your message has been sent!');
+            return $this->redirectToRoute('contact');
+        }
 
+        if (($result['error'] ?? null) === 'too_fast') {
+            $this->addFlash('warning', 'Please wait a few seconds before submitting.');
             return $this->redirectToRoute('contact');
         }
 
