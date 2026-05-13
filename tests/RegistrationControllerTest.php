655b4f11f936a0dd36e84b79668e1c0d5726e11f
M-Laetitia <molin.laetitia@gmail.com>
2026-05-13 08:42:49 +0200

    feat: update registration page title to 'Create Account'

--- tests/RegistrationControllerTest.php
+++ tests/RegistrationControllerTest.php
@@ -36,7 +36,7 @@ class RegistrationControllerTest extends WebTestCase
         // Register a new user
         $this->client->request('GET', '/register');
         self::assertResponseIsSuccessful();
-        self::assertPageTitleContains('Register');
+        self::assertPageTitleContains('Create Account');
 
         $this->client->submitForm('Register', [
             'registration_form[email]' => 'me@example.com',
